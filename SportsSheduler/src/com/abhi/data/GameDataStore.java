a340b5d387f27609381a9c7208260379efdfb7e4
Abhijeet Pandit <abhijeetpandit2008@gmail.com>
2014-06-04 17:03:59 +0530

    Added data store class

---
+++ SportsSheduler/src/com/abhi/data/GameDataStore.java
@@ -0,0 +1,14 @@
+package com.abhi.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.abhi.beans.Day;
+import com.abhi.beans.Game;
+import com.abhi.beans.Player;
+
+public class GameDataStore {
+	private List<Day> days = new ArrayList<>();
+	private List<Game> games = new ArrayList<>();
+	private List<Player> players = new ArrayList<>();
+}
